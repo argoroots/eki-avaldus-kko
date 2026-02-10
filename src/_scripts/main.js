e210ca3a39af5c220c94afbc94bd4775bf5935c7
Argo Roots <argo@roots.ee>
2026-02-10 16:11:45 +0200

    Replace plausible calls with window.analytics.track

--- src/_scripts/main.js
+++ src/_scripts/main.js
@@ -3,7 +3,7 @@ $(function () {
         $('#form').addClass('d-none')
         $('#uploading').removeClass('d-none')
 
-        plausible('Submit')
+        window.analytics.track('submit')
 
         var properties = [
             { type: '_type', reference: '66603579bb3ad6bf9b82a893' }, // schoolbook_application
@@ -102,7 +102,7 @@ $(function () {
             filetype: file.type
         }]
 
-        plausible('Upload')
+        window.analytics.track('upload')
 
         $.ajax({
             method: 'POST',
